705ca8fbd0c7d38c4f983bfcb36537acffa8c731
RedwanulKarim612 <1805111@ugrad.cse.buet.ac.bd>
2023-08-21 02:23:03 +0600

    Fixture: select gameweek

--- src/Pages/User/FixturePage.jsx
+++ src/Pages/User/FixturePage.jsx
@@ -1,28 +1,59 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar";
-import { Card, Grid, Typography } from "@mui/material";
+import { Card, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 import { getFixtureDetails } from "../../api/User";
 import { useNavigate } from "react-router-dom";
-
+import _ from "lodash";
 const FixturePage = () =>{
     const qlink = window.location.href;
     const tokens = qlink.split('/');
     const gw = tokens[tokens.length-1];
     const [fixtureDetails, setFixtureDetails] = useState(null);
+    const [gameweek, setGameweek] = useState(1);
     const navigate = useNavigate();
     useEffect(()=>{
         getFixtureDetails(gw).then((res)=>{
             console.log(res);
             setFixtureDetails(res);
+            setGameweek(gw);
         })
     }, [])
     if(!fixtureDetails){
         return <div>Loading...</div>
     }
+
+    const handleChangeGameWeek = (event) => {
+        console.log(event.target.value);
+        navigate("/fixture/"+event.target.value);
+        navigate(0)
+    }
     return(
         <div>
             <Navbar/>
             <div style={{width: '70%', margin: '30px auto'}}>
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Typography variant="h6">
+                        Gameweek
+                    </Typography>
+                <FormControl variant="standard" sx={{ m: 1}}>
+                    {/* <InputLabel id="demo-simple-select-required-label">Gameweek</InputLabel> */}
+                    <Select
+                    labelId="demo-simple-select-required-label"
+                    id="demo-simple-select-required"
+                    value={gameweek}
+                    label="Gameweek"
+                    onChange={handleChangeGameWeek}
+                    >{
+                        _.range(1,39).map((gw)=>{
+                            return <MenuItem value={gw}>
+                                <Typography variant="h6">{gw}</Typography>
+                                {/* {gw} */}
+                            </MenuItem>
+                        })
+                    }
+                    </Select>
+                </FormControl>
+                </div>
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8}}>
                 {fixtureDetails.matches.map((row, index)=>{
                     return (
