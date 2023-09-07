c6f4709f9ce7d5b7f831d5ad8d2170bbc8c61a14
RedwanulKarim612 <1805111@ugrad.cse.buet.ac.bd>
2023-09-08 04:32:00 +0600

    FL: take necessary inputs

--- src/Pages/User/FLCreate.jsx
+++ src/Pages/User/FLCreate.jsx
@@ -1,11 +1,12 @@
-import {React} from "react";
+import {React, useState} from "react";
 import Navbar from "../../Components/Navbar";
-import { FormControl, TextField, Typography } from "@mui/material";
+import { Button, FormControl, TextField, Typography } from "@mui/material";
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateField } from '@mui/x-date-pickers/DateField';
 import { DatePicker, TimePicker } from "@mui/x-date-pickers";
+import dayjs from 'dayjs';
 
 const FieldHeader = (props)=>{
     return(
@@ -21,6 +22,28 @@ const FieldHeader = (props)=>{
 }
 
 const FLCreate = () => {
+    const [name, setName] = useState("");
+    const [min_teams, setMinTeams] = useState(0);
+    const [max_teams, setMaxTeams] = useState(0);
+    const [team_player_count, setTeamPlayerCount] = useState(0);
+    const [start_date, setStartDate] = useState("");
+    const [match_time, setMatchTime] = useState("");
+    const [allow_auto_join, setAllowAutoJoin] = useState(false);
+    const [timeZone, setTimeZone] = useState("");    
+    const handleCreate = () => {
+        console.log(dayjs(start_date).format('DD-MM-YYYY'))
+        let league = {
+            name: name,
+            min_teams: min_teams,
+            max_teams: max_teams,
+            team_player_count: team_player_count,
+            start_date: dayjs(start_date).format('DD-MM-YYYY'),
+            match_time: dayjs(match_time).format('HH:mm'),
+            allow_auto_join: allow_auto_join,
+            time_zone: timeZone
+        }
+        console.log(league);
+    }
     return (
         <div>
             <Navbar/>
@@ -28,25 +51,25 @@ const FLCreate = () => {
                 <FormControl>
                     <div style={{display:"flex",flexDirection:"row", justifyContent: "space-between"}}>
                         <FieldHeader title="League Name"/>
-                        <TextField required id="outlined-required"/>
+                        <TextField required id="outlined-required" onChange={(event)=>{setName(event.target.value)}}/>
                     </div>
                     <div style={{display:"flex",flexDirection:"row", justifyContent: "space-between"}}>
                     <FieldHeader title="Min Teams"/>
-                        <TextField required id="outlined-required" type="number"/>
+                        <TextField required id="outlined-required" type="number" onChange={(event)=>{setMinTeams(event.target.value)}}/>
                     </div>
-                    <div style={{display:"flex",flexDirection:"row", justifyContent: "space-between"}}>
+                    <div style={{display:"flex",flexDirection:"row", justifyContent: "space-between"}} >
                     <FieldHeader title="Max Teams"/>
-                        <TextField required id="outlined-required" type="number"/>
+                        <TextField required id="outlined-required" type="number" onChange={(event)=>{setMaxTeams(event.target.value)}}/>
                     </div>
                     <div style={{display:"flex",flexDirection:"row", justifyContent: "space-between"}}>
                     <FieldHeader title="Players in team"/>
-                        <TextField required id="outlined-required" type="number"/>
+                        <TextField required id="outlined-required" type="number" onChange={(event)=>{setTeamPlayerCount(event.target.value)}}/>
                     </div>
                     <div style={{display:"flex",flexDirection:"row", justifyContent: "space-between"}}>
                     <FieldHeader title="Start Date"/>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DemoContainer components={['DateField']}>
-                        <DatePicker></DatePicker>
+                        <DatePicker onChange={(newVal)=>{setStartDate(newVal)}}/>
                         </DemoContainer>
                     </LocalizationProvider>
                     </div>
@@ -54,13 +77,16 @@ const FLCreate = () => {
                     <FieldHeader title="Match Time"/>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DemoContainer components={['TimePicker']}>
-                            <TimePicker/>
+                            <TimePicker onChange={(newVal)=>{setMatchTime(newVal)}}/>
                         </DemoContainer>
                     </LocalizationProvider>
                     </div>
 
                     <div style={{display:"flex",flexDirection:"row"}}>
-                        <input type="checkbox"/>Allow Auto-join
+                        <input type="checkbox" onChange={(event)=>{setAllowAutoJoin(event.target.value)}}/>Allow Auto-join
+                    </div>
+                    <div style={{display:"flex",flexDirection:"row", margin:"auto"}}>
+                        <Button variant="contained" onClick={()=>{handleCreate()}}>Create</Button>
                     </div>
                 </FormControl>
             </div>
